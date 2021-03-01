06f6121e45c050519ee8c17f79fd53b16f5cfda3
manal shehri <manal@intern.tera-cit.net>
2021-03-01 06:07:59 +0300

    DB config

---
+++ config.py
@@ -0,0 +1,19 @@
+import os
+
+class DatabaseURI:
+    SECRET_KEY = os.urandom(32)
+
+    # Grabs the folder where the script runs.
+    basedir = os.path.abspath(os.path.dirname(__file__))
+
+    # Enable debug mode.
+    DEBUG = True
+
+    # declear the DB URI parts
+    DATABASE_NAME = "casting_agency"
+    username = 'manal'
+    password = '123456m'
+    url = 'localhost:5432'
+
+    # Connect to the database
+    SQLALCHEMY_DATABASE_URI = "postgres://{}:{}@{}/{}".format(username, password, url, DATABASE_NAME)
